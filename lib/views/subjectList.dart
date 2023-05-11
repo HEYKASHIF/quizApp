815863b38e666f95d453c2235ac2efdc976d5999
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-11 23:00:31 +0530

    11-may commit1

---
+++ lib/views/subjectList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class subjectList extends StatelessWidget {
+  const subjectList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('QUIZ'),
+      ),
+    );
+  }
+}
