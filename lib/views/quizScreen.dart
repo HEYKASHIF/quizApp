ae6db67e3e2ecec25867f0fc1167597ab7a929df
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-11 23:34:13 +0530

    11-may commit8

--- lib/views/quizScreen.dart
+++ lib/views/quizScreen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class quizScreen extends StatefulWidget {
+  const quizScreen({super.key});
+
+  @override
+  State<quizScreen> createState() => _quizScreenState();
+}
+
+class _quizScreenState extends State<quizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
