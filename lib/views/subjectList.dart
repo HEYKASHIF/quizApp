25107e22e1894c1464ce4852e306af2ed9c69b18
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-11 23:26:02 +0530

    11-may commit6

--- lib/views/subjectList.dart
+++ lib/views/subjectList.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class subjectList extends StatelessWidget {
   const subjectList({super.key});
   final List subjectListData = const [
-    {'subject_name': 'hindi', 'subject_id': '1001'},
+    {'subject_name': 'Hindi', 'subject_id': '1001'},
     {'subject_name': 'English', 'subject_id': '1002'},
     {'subject_name': 'Science', 'subject_id': '1003'},
     {'subject_name': 'Math', 'subject_id': '1004'},
@@ -21,13 +21,17 @@ class subjectList extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            ListTile(
-              tileColor: Colors.teal,
-              textColor: Colors.white,
-              iconColor: Colors.white,
-              title: Text('HINDI'),
-              trailing: Icon(Icons.arrow_circle_right),
-            )
+            ...subjectListData
+                .map(
+                  (e) => ListTile(
+                    tileColor: Colors.teal,
+                    textColor: Colors.white,
+                    iconColor: Colors.white,
+                    title: Text(e['subject_name']),
+                    trailing: Icon(Icons.arrow_circle_right),
+                  ),
+                )
+                .toList()
           ],
         ),
       ),
