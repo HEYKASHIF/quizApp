8dd631319ca1a713cc24ee6dec6fdfd5f42430ad
HEYKASHIF <kashifmohd269@gmail.com>
2023-06-07 22:55:55 +0530

    "7-june commit1"

--- lib/bootcamp.dart
+++ lib/bootcamp.dart
@@ -18,7 +18,7 @@ class bootCamp extends StatelessWidget {
             height: 100,
             width: 250,
             color: Color.fromARGB(255, 171, 6, 216),
-            child: Center(child: Text('Hyy Kashif')),
+            child: Center(child: Text('Hyy mohd Kashif')),
           ),
           Container(
             height: 100.0,
